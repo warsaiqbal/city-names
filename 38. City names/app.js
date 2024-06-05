edc524aee46ba4fdafff8e6c43beb8be0a3d1012
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:36:57 -0700

    Add files via upload

---
+++ 38. City names/app.js
@@ -0,0 +1,6 @@
+function city_country(city, country) {
+    console.log("".concat(city, ", ").concat(country, "."));
+}
+city_country("Lahore", "Pakistan");
+city_country("London", "England");
+city_country("New york", "USA");
---
+++ 38. City names/app.ts
@@ -0,0 +1,7 @@
+function city_country(city: string, country: string){
+    console.log(`${city}, ${country}.`);
+}
+
+city_country("Lahore", "Pakistan")
+city_country("London", "England")
+city_country("New york", "USA")
